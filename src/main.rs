93b24d172a67b52e2c1c18e57b4a7cd3d5b30779
Mason Stallmo <masonstallmo@hey.com>
2025-03-11 14:33:08 -0700

    feat: fixed-ish, working-ish terminal image viewer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,78 @@
-use color_eyre::Result;
+use anyhow::{Error as E, Result};
+use image::DynamicImage;
 use ratatui::crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::widgets::Block;
 use ratatui::{DefaultTerminal, Frame, prelude::*};
-use std::{env, fs, path::Path};
+use ratatui_image::{Resize, StatefulImage, picker::Picker};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 use tui_tree_widget::{Tree, TreeItem, TreeState};
 
+struct AppState<'a> {
+    pub tree_items: Vec<TreeItem<'a, String>>,
+    pub tree_state: TreeState<String>,
+    pub picker: Picker,
+    pub image: Option<DynamicImage>,
+}
+
+impl<'a> AppState<'a> {
+    fn new() -> Result<Self> {
+        let picker = Picker::from_query_stdio()?;
+        let mut items = vec![];
+        let _ = read_all(env::current_dir()?, &mut items);
+
+        Ok(Self {
+            tree_state: TreeState::default(),
+            tree_items: items,
+            picker,
+            image: None,
+        })
+    }
+}
+
+fn read_all<'a, P: AsRef<Path>>(path: P, items: &mut Vec<TreeItem<'a, String>>) -> Result<()> {
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        if entry.metadata()?.is_dir() {
+            let mut sub_items = vec![];
+            let _ = read_all(entry.path(), &mut sub_items)?;
+            if sub_items.len() > 0 {
+                let item = TreeItem::new(
+                    entry.path().to_string_lossy().to_string(),
+                    entry.path().to_string_lossy().to_string(),
+                    sub_items,
+                )?;
+                items.push(item);
+            }
+        } else {
+            if is_image(&entry.path()) {
+                let item = TreeItem::new_leaf(
+                    entry.file_name().to_string_lossy().to_string(),
+                    entry.path().to_string_lossy().to_string(),
+                );
+                items.push(item);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn is_image(path: &Path) -> bool {
+    match path.extension() {
+        Some(extension) => match extension.to_str() {
+            Some("jpg") | Some("jpeg") | Some("png") => true,
+            _ => false,
+        },
+        None => false,
+    }
+}
+
 fn main() -> Result<()> {
-    color_eyre::install()?;
-    let state = AppState::default();
+    color_eyre::install().map_err(E::msg)?;
+    let state = AppState::new()?;
 
     let terminal = ratatui::init();
     let result = run(terminal, state);
@@ -16,11 +81,19 @@ fn main() -> Result<()> {
 }
 
 fn run(mut terminal: DefaultTerminal, mut state: AppState) -> Result<()> {
+    if let Some(first_item) = state.tree_items.first() {
+        state
+            .tree_state
+            .select(vec![first_item.identifier().to_owned()]);
+    }
+
     loop {
+        // Render view
         terminal.draw(|frame| {
             render(frame, &mut state);
         })?;
 
+        // Handle events
         match event::read()? {
             Event::Key(key_event) => {
                 if key_event.kind == KeyEventKind::Press {
@@ -35,50 +108,22 @@ fn run(mut terminal: DefaultTerminal, mut state: AppState) -> Result<()> {
             }
             _ => {}
         }
+
+        // Update state
+        let selected_path = PathBuf::from(state.tree_state.selected().join("/"));
+        if let Some(extension) = selected_path.extension() {
+            if extension.to_str() == Some("jpg") && state.image.is_none() {
+                let image = image::ImageReader::open(selected_path)?.decode()?;
+                state.image = Some(image);
+            } else {
+                state.image = None;
+            }
+        } else {
+            state.image = None;
+        }
     }
 
     Ok(())
-}
-
-struct AppState<'a> {
-    pub tree_items: Vec<TreeItem<'a, String>>,
-    pub tree_state: TreeState<String>,
-}
-
-impl<'a> Default for AppState<'a> {
-    fn default() -> Self {
-        let mut items = vec![];
-        read_all(env::current_dir().unwrap(), &mut items);
-
-        Self {
-            tree_state: TreeState::default(),
-            tree_items: items,
-        }
-    }
-}
-
-// TODO: Fix unwrapping
-fn read_all<'a, P: AsRef<Path>>(path: P, items: &mut Vec<TreeItem<'a, String>>) {
-    for entry in fs::read_dir(path).unwrap() {
-        let entry = entry.unwrap();
-        if entry.metadata().unwrap().is_dir() {
-            let mut sub_items = vec![];
-            read_all(entry.path(), &mut sub_items);
-            let item = TreeItem::new(
-                entry.path().to_string_lossy().to_string(),
-                entry.path().to_string_lossy().to_string(),
-                sub_items,
-            )
-            .unwrap();
-            items.push(item);
-        } else {
-            let item = TreeItem::new_leaf(
-                entry.file_name().to_string_lossy().to_string(),
-                entry.path().to_string_lossy().to_string(),
-            );
-            items.push(item);
-        }
-    }
 }
 
 // Main render function
@@ -88,9 +133,8 @@ fn render(frame: &mut Frame, state: &mut AppState) {
         .constraints(vec![Constraint::Percentage(25), Constraint::Percentage(75)])
         .split(frame.area());
 
-    // frame.render_widget("hello world", layout[0]);
     file_explorer(frame, layout[0], state);
-    frame.render_widget(state.tree_state.selected().join("/"), layout[1]);
+    image_viewer(frame, layout[1], state);
 }
 
 fn file_explorer(frame: &mut Frame, area: Rect, state: &mut AppState) {
@@ -105,4 +149,17 @@ fn file_explorer(frame: &mut Frame, area: Rect, state: &mut AppState) {
         )
         .highlight_symbol(">> ");
     frame.render_stateful_widget(tree_widget, area, &mut state.tree_state);
+}
+
+fn image_viewer(frame: &mut Frame, area: Rect, state: &mut AppState) {
+    if let Some(image) = &state.image {
+        let mut image_state = state.picker.new_resize_protocol(image.clone());
+        let image_widget = StatefulImage::new();
+
+        frame.render_stateful_widget(
+            image_widget.resize(Resize::Crop(None)),
+            area.inner(Margin::new(5, 2)),
+            &mut image_state,
+        );
+    }
 }
